9f35f5523393925c957dbfc45e5d46d4f13553a4
MaartenBrijker <maartenbrijker@gmail.com>
2016-05-11 17:26:47 +0200

    simple first one

---
+++ MultipleToDo/CustomCell.swift
@@ -0,0 +1,22 @@
+//
+//  CustomCell.swift
+//  MultipleToDo
+//
+//  Created by Maarten Brijker on 11/05/16.
+//  Copyright © 2016 Maarten_Brijker. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+    
+    @IBOutlet weak var toDoLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
---
+++ MultipleToDo/ToDoManager.swift
@@ -0,0 +1,22 @@
+//
+//  ToDoManager.swift
+//  MultipleToDo
+//
+//  Created by Maarten Brijker on 11/05/16.
+//  Copyright © 2016 Maarten_Brijker. All rights reserved.
+//
+
+import Foundation
+
+class ToDoManager {
+    
+    static let sharedInstance = ToDoManager()
+    
+    private init() { }
+    
+    func printHello() {
+        print("hello hello")
+    }
+    
+    
+}
